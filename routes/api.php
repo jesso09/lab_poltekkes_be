5819c50ec49bc5f558054f710ee88711e2e68f14
Gede Bagus Jyestha Permana <jyestha47@gmail.com>
2024-10-05 00:02:27 +0800

    .

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::group(['prefix' => 'auth'], function () {
 Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum']], function () {
     Route::get('user', [AuthController::class, 'indexUser']);
     Route::post('status/{nipOrUsername}', [AuthController::class, 'changeStatusUser']);
+    Route::get('status/{idUser}', [AuthController::class, 'getUserToken']);
 });
 
 // Route::group(['prefix' => 'auth', 'middleware' => ['auth:sanctum']], function () {
